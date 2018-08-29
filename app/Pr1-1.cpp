47cdf9e6f44646ca62b088745b67402046a5fd02
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-08-29 11:47:31 -0500

    Student template for C++ implementation

--- app/Pr1-1.cpp
+++ app/Pr1-1.cpp
@@ -6,18 +6,19 @@ int main()
 {
    double hours, rate, pay;
 
-   // Get the number of hours worked.
-   cout << "How many hours did you work? ";
-   cin >> hours;
-   
-   // Get the hourly pay rate.
-   cout << "How much do you get paid per hour? ";
-   cin >> rate;
-   
+   // Prompt "How many hours did you work? " to console
+
+   // From keyboard, get the number of hours worked and set the value into variable hours
+
+   // Prompt "How much do you get paid per hour? " to console
+
+   // From keyboard, get the hourly pay rate and set the value into variable rate
+
    // Calculate the pay.
-   pay = hours * rate;
+
    
    // Display the pay.
-   cout << "You have earned $" << pay << endl;
-   return 0; 
+
+   // return value 0 to the caller of main
+
 }
